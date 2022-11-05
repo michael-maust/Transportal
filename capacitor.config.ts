92782ec51df66972982110c403ecd2678e72c0b5
Michael Maust <info@michaelmaust.com>
2022-11-05 12:01:41 -0500

    Add native SDKs

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'Transportal.app',
+  appName: 'Transportal',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
