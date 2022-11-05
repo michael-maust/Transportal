f588c2519e85203d60ed9b49f512dd386fc735ef
Michael Maust <info@michaelmaust.com>
2022-11-05 08:30:28 -0500

    Adds additional routes

--- src/index.js
+++ src/index.js
@@ -4,16 +4,24 @@ import "./index.css";
 import App from "./App";
 import {BrowserRouter, Routes, Route, Link} from "react-router-dom";
 import Login from "./routes/login";
-import Dashboard from "./routes/dashboard";
+import Signup from "./routes/signup";
+import Welcome from "./routes/welcome";
+import Home from "./routes/home";
+import RouteList from "./routes/routeList";
+import RestSpots from "./routes/restSpots";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />}>
-          <Route path="" element={<Login />} />
-          <Route path="/app/dashboard" element={<Dashboard />} />
+        <Route path="/" exact element={<App />}>
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/home" exact element={<Home />} />
+          <Route path="/signup" exact element={<Signup />} />
+          <Route path="/welcome" exact element={<Welcome />} />
+          <Route path="/routes" exact element={<RouteList />} />
+          <Route path="/rest" exact element={<RestSpots />} />
           <Route
             path="*"
             element={
---
+++ src/routes/routeList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function RouteList() {
+  return <div>routes</div>;
+}
