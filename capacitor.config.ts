e92e20f1eea1527a103059d598b1247d9d557dd5
Michael Maust <info@michaelmaust.com>
2022-11-04 20:26:35 -0500

    Add ip address to server

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,14 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import {CapacitorConfig} from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'Transportal.app',
-  appName: 'Transportal',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "Transportal.app",
+  appName: "Transportal",
+  webDir: "build",
+  bundledWebRuntime: false,
+  server: {
+    url: "http://192.168.172.110:3000",
+    cleartext: true,
+  },
 };
 
 export default config;
