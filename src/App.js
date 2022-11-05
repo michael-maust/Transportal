1cbade41ea8ced70e057e48d8aeda17da84b7914
Luke Sharon <lukedsharon@gmail.com>
2022-11-05 12:46:52 -0500

    last

--- src/App.js
+++ src/App.js
@@ -48,19 +48,33 @@ import React from "react";
 
 setupIonicReact();
 
-const Settings = () => <div>Hello from Settings!</div>;
+const Settings = () => (
+  <IonPage>
+    <IonHeader>
+      <IonToolbar>
+        <IonTitle>Settings</IonTitle>
+      </IonToolbar>
+    </IonHeader>
+    <IonContent>Settings Page and Content</IonContent>
+  </IonPage>
+);
 
 function App() {
-  const [user, setUser] = useState("");
-  const [session, setSession] = useState("");
   return (
     <div>
       <div>Hello World</div>
       <IonApp>
         <IonReactRouter>
           <IonRouterOutlet>
-            <Route path="" component={Settings} />
-            <Route path="/login" component={Login} />
+            <Route path="/" exact={true}>
+              <Redirect to="/dashboard" />
+            </Route>
+            <Route path="/homse" exact={true}>
+              <Login />
+            </Route>
+            <Route path="/settings" exact={true}>
+              <Settings />
+            </Route>
           </IonRouterOutlet>
         </IonReactRouter>
       </IonApp>
