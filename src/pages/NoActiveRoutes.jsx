d04be6cef2774d06717c82ec6a1b38dfb9aa10e5
Michael Maust <info@michaelmaust.com>
2022-11-05 18:40:39 -0500

    Create NoActiveRoutes.jsx

---
+++ src/pages/NoActiveRoutes.jsx
@@ -0,0 +1,93 @@
+import React, {useRef, useState} from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonButton,
+  IonModal,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonAvatar,
+  IonImg,
+  IonSearchbar,
+} from "@ionic/react";
+import routePlaceholder from "../assets/pictures/routePlaceholder";
+import CreateRouteModal from "../components/createRouteModal";
+
+const RouteData = [];
+
+const NoActiveRoutesFound = () => {
+
+  return (
+    <ion-content
+      class="ion-padding"
+      style={{overflow: "hidden"}}
+      fullscreen={true}
+    >
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+          gap: "8px",
+          width: "full",
+          height: "100%",
+          position: "relative",
+        }}
+      >
+        {routePlaceholder}
+        <p
+          style={{
+            maxWidth: "400px",
+            textAlign: "center",
+            fontFamily: "Roboto-Medium",
+            fontSize: "32px",
+            padding: 0,
+            margin: "12px",
+          }}
+        >
+          No Routes Found
+        </p>
+
+        <IonButton
+          onClick={() => setIsOpen((currentValue) => !currentValue)}
+          color="primary"
+          style={{
+            width: "200px",
+            position: "relative",
+          }}
+          id="open-modal"
+        >
+          Add Route
+        </IonButton>
+
+        <IonModal
+          isOpen={isOpen}
+          initialBreakpoint={0.25}
+          breakpoints={[0, 0.25, 0.5, 0.25]}
+          style={{
+            width: "80%",
+            height: "30%",
+            minHeight: 0,
+            maxHeight: "250px",
+            top: "50%",
+            bottom: 0,
+            left: "10%",
+            right: "10%",
+            paddingTop: "100px",
+          }}
+        >
+          <IonContent className="ion-padding" style={{height: "400px"}}>
+            <div className="">TEST</div>
+          </IonContent>
+        </IonModal>
+      </div>
+    </ion-content>
+  );
+};
+
+export default NoActiveRoutesFound;
